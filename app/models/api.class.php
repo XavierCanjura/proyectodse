90b40e622b77482a11a31b9deead0f2eb214daf1
XavierCanjura <36569554+XavierCanjura@users.noreply.github.com>
2023-11-17 21:59:48 -0600

    fix get function

--- app/models/api.class.php
+++ app/models/api.class.php
@@ -1,23 +1,46 @@
 <?php
     class API{       
         public function get($url, $token){
-            $options = [
-                'http' => [
-                    'method' => 'GET',
-                    'header' => array("Accept: application/json", "Content-Type: application/json", "Authorization: Bearer $token"),
-                ],
-            ];
+            // $options = [
+            //     'http' => [
+            //         'method' => 'GET',
+            //         'header' => array("Accept: application/json", "Content-Type: application/json", "Authorization: Bearer $token"),
+            //     ],
+            // ];
 
-            $context = stream_context_create($options);
-            $result = file_get_contents($url, false, $context);
+            // $context = stream_context_create($options);
+            // $result = file_get_contents($url, false, $context);
 
-            if(!$result){
-                echo "Error\n";
+            // if(!$result){
+            //     echo "Error\n";
+            // }
+
+            // $json = json_decode($result);
+
+            // return $json;
+
+            $ch = curl_init($url);
+
+            curl_setopt_array($ch, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => array(
+                    "Accept: application/json", 
+                    "Content-Type: application/json",
+                    "Authorization: Bearer $token",
+                ),
+                // CURLOPT_POSTFIELDS => json_encode($dataForm),
+            ));
+
+            $response = curl_exec($ch);
+            curl_close($ch);
+
+            if(!$response) {
+                return false;
+            }else{
+                return json_decode($response);
             }
-
-            $json = json_decode($result);
-
-            return $json;
 
         }
 
